3199434ebad91e69e76f3f23615fee7fd9cbcaf7
Daniel Janowski <daniel.janowski@gmail.com>
2020-02-17 16:08:29 +0000

    first commit

---
+++ precourse_recap.rb
@@ -0,0 +1,14 @@
+1.
+if x < 10
+  puts "The number is smaller than 10"
+else
+  puts "Number is bigger or equal than 10"
+end
+
+2.
+def multiply number1, number2
+  result = number1 * number2
+  puts "Result is: #{result}"
+end
+
+3.
