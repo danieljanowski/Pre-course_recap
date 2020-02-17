90c7e1268213241a57017e3403081f51ff1cfac9
Daniel Janowski <daniel.janowski@gmail.com>
2020-02-17 16:25:55 +0000

    update

--- precourse_recap.rb
+++ precourse_recap.rb
@@ -1,14 +1,17 @@
-1.
+# 1.
 if x < 10
   puts "The number is smaller than 10"
 else
   puts "Number is bigger or equal than 10"
 end
 
-2.
+# 2.
 def multiply number1, number2
   result = number1 * number2
   puts "Result is: #{result}"
 end
 
-3.
+# 3.
+array = ["first", "second", "third", "forth"]
+
+# 4.
