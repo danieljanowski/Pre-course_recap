6837a8778c352274645253f38df32b53eaa1d815
Daniel Janowski <daniel.janowski@gmail.com>
2020-02-17 17:26:51 +0000

    all the examples added

--- precourse_recap.rb
+++ precourse_recap.rb
@@ -1,5 +1,6 @@
 # 1.
-if x < 10
+number = 7
+if number < 10
   puts "The number is smaller than 10"
 else
   puts "Number is bigger or equal than 10"
@@ -11,7 +12,20 @@ def multiply number1, number2
   puts "Result is: #{result}"
 end
 
+multiply 10, 40
+
 # 3.
 array = ["first", "second", "third", "forth"]
+p array
 
 # 4.
+capital_cities = { "Scotland" => "Edinburgh", "Germany" => "Berlin", "Poland" => "Warsaw" }
+p capital_cities
+
+# 5.
+i = 1
+j = 20
+while i < j
+  print "#{i}, "
+  i += 1
+end
